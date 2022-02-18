33eab230eb9469de124bd7dba781ecc1e4033a26
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-18 03:46:19 +0300

    Scripted auto-commit on change (2022-02-18 03:46:19) by gitwatch.sh

--- src/main/java/lab3/Main.java
+++ src/main/java/lab3/Main.java
@@ -39,7 +39,7 @@ public class Main {
                 System.out.println(cols[19]);
                 String destination = cols[14];
                 float timeOfDelay = cols[18].isEmpty() ? 0 : Float.parseFloat(cols[18]);
-                return new Tuple2<>(new Tuple2<>(departure,destination),new Flight(departure,destination,cancelled,timeOfDelay));
+                return new Tuple2<>(new Tuple2<>(departure,destination),new Flight(departure,destination,cancelled, timeOfDelay));
             });
     }
 
