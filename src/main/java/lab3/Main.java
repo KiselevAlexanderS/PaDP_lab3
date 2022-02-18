40c2719678279fdf8fc185f770063563417bb088
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-18 03:35:14 +0300

    Scripted auto-commit on change (2022-02-18 03:35:14) by gitwatch.sh

--- src/main/java/lab3/Main.java
+++ src/main/java/lab3/Main.java
@@ -5,15 +5,15 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class Main {
-    private JavaRDD<String> flights;
-    private JavaRDD<String> airports;
-    
+    private static JavaRDD<String> flights;
+    private static JavaRDD<String> airports;
+
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext context = new JavaSparkContext(conf);
 
-//        JavaRDD<String> flights = context.textFile(args[0]);
-//        JavaRDD<String> airports = context.textFile(args[1]);
+        flights = context.textFile(args[0]);
+        airports = context.textFile(args[1]);
         JavaRDD<String> finalFlights = flights;
         JavaRDD<String> finalAirports = airports;
 
