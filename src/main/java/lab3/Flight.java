23c13e34ea1e76576932ebc8cab0449123b6095b
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-18 03:23:32 +0300

    Scripted auto-commit on change (2022-02-18 03:23:32) by gitwatch.sh

--- src/main/java/lab3/Flight.java
+++ src/main/java/lab3/Flight.java
@@ -10,8 +10,8 @@ public class Flight implements Serializable {
 
     Flight(String Depature, String Arrival, boolean Canceled, int Delay) {
         this.Depature = Depature;
-        this.Depature = Depature;
-        this.Depature = Depature;
-        this.Depature = Depature;
+        this.Arrival = Arrival;
+        this.Canceled = Canceled;
+        this.Delay = Delay;
     }
 }
