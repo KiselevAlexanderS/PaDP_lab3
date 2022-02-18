d24fa07804c5a8b0fff8b31db46e895820db6f74
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-18 03:19:34 +0300

    Scripted auto-commit on change (2022-02-18 03:19:34) by gitwatch.sh

--- src/main/java/lab3/Main.java
+++ src/main/java/lab3/Main.java
@@ -1,12 +1,15 @@
 package lab3;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class Main {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext context = new JavaSparkContext(conf);
+        JavaRDD<String> flights = context.textFile(args[0]);
+        JavaRDD<String> airports = context.textFile(args[1]);
     }
 
 }
