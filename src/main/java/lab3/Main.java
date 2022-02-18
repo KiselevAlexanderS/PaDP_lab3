b7cbb205b1f9f339bfe4061c3e97cd4c44c0d5de
KiselevAlexanderS <kpacnaypanda@gmail.com>
2022-02-18 03:54:51 +0300

    Scripted auto-commit on change (2022-02-18 03:54:51) by gitwatch.sh

--- src/main/java/lab3/Main.java
+++ src/main/java/lab3/Main.java
@@ -4,6 +4,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.broadcast.Broadcast;
 import scala.Tuple2;
 
 public class Main {
@@ -11,6 +12,27 @@ public class Main {
     private static JavaRDD<String> airports;
     private static JavaPairRDD<String,String> airport;
     private static JavaPairRDD<Tuple2<String,String>, Flight> flight;
+
+    private static void map_to_pair() {
+        airport = airports.mapToPair(
+                line -> {
+                    String[] cols = line.split(",");
+                    String code = cols[0].replace("\"","");
+                    String description = cols[1].replace("\"","");
+                    return new Tuple2<>(code, description);
+                });
+
+        flight = flights.mapToPair(
+                line -> {
+                    String[] cols = line.split(",");
+                    boolean cancelled = Float.parseFloat(cols[19]) != 0;
+                    String departure = cols[11];
+                    System.out.println(cols[19]);
+                    String destination = cols[14];
+                    float timeOfDelay = cols[18].isEmpty() ? 0 : Float.parseFloat(cols[18]);
+                    return new Tuple2<>(new Tuple2<>(departure,destination),new Flight(departure,destination,cancelled, timeOfDelay));
+                });
+    }
 
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("lab3");
@@ -23,26 +45,9 @@ public class Main {
         flights = flights.filter(a -> !a.equals(finalFlights));
         airports = airports.filter(a -> !a.equals(finalAirports));
 
-        airport = airports.mapToPair(
-            line -> {
-                String[] cols = line.split(",");
-                String code = cols[0].replace("\"","");
-                String description = cols[1].replace("\"","");
-                return new Tuple2<>(code, description);
-            });
+        map_to_pair();
 
-        flight = flights.mapToPair(
-            line -> {
-                String[] cols = line.split(",");
-                boolean cancelled = Float.parseFloat(cols[19]) != 0;
-                String departure = cols[11];
-                System.out.println(cols[19]);
-                String destination = cols[14];
-                float timeOfDelay = cols[18].isEmpty() ? 0 : Float.parseFloat(cols[18]);
-                return new Tuple2<>(new Tuple2<>(departure,destination),new Flight(departure,destination,cancelled, timeOfDelay));
-            });
-
-        
+        final Broadcast<Map<String,String>> airBrodcast = context.broadcast(airport.collectAsMap());
     }
 
 }
